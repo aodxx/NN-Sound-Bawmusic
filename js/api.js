d80add166dcba2e52ef739d978562b1e263e3245
aodxx <aodaod3826@gmail.com>
2026-07-18 09:23:50 +0800

    Update API_URL to new Google Apps Script URL

--- js/api.js
+++ js/api.js
@@ -4,7 +4,7 @@
  */
 
 // ⚠️ แก้ URL นี้เป็น Web App URL ของคุณหลัง Deploy Apps Script (ดู INSTALL.md)
-const API_URL = 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbykgZIfxTQ3652WNWaES0ZPWubbv62IlXYqgar27u-S8a0nNZ8yiinFj_BZd8Dg1ow5/exec';
 
 const BawmusicAPI = {
   async call(action, params = {}, isPost = false) {
